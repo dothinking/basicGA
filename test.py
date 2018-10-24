f69b9d35db55eaeea4e219fe3fb6b76a8bf2df30
dothinking <train8808@gmail.com>
2018-10-24 23:24:27 +0800

    test cases

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+from GA import GA
+import math
+import numpy as np
+
+# ------------------
 # testing functions
+# ------------------
+# [-2,1]
+f1 = lambda x: x[0]*math.sin(10*np.pi*x[0])+2
 
 # sol: x=[0,0], min=0
-f = lambda x : 20 + x[0]**2 + x[1]**2 - 10*(math.cos(2*np.pi*x[0])+math.cos(2*np.pi*x[1]))
+f2 = lambda x : 20 + x[0]**2 + x[1]**2 - 10*(math.cos(2*np.pi*x[0])+math.cos(2*np.pi*x[1]))
 
 # schaffer
 # sol: x=[0,0], min=0
@@ -23,17 +31,21 @@ def shubert(x):
 # sol: x=[1,1], min=0
 rosenbrock = lambda x: 100*(x[1]-x[0]**2)**2+(1-x[0])**2
 
+# ------------------
 # test GA
+# ------------------
 def test(obj, sol):
 	# GA res
 	kw = {
 		'lbound': [-100, -100],
 		'ubound': [100, 100],
-		'size'	: 500,
-		'max_generation': 50,
-		'crossover_rate': 0.9,
-		'mutation_rate'	: 0.1,
-		'crossover_alpha': 0.25
+		'size'	: 195,
+		'max_generation': 30,
+		'fitness': lambda x: np.exp(-x),
+		# 'selection_mode': 'elite',
+		'crossover_rate': 0.85,		
+		'crossover_alpha': 0.75,
+		'mutation_rate'	: 0.02
 	}
 	g = GA(obj, 2, **kw)
 	_,res = g.solve()
@@ -48,7 +60,7 @@ def test(obj, sol):
 
 if __name__ == '__main__':
 
-	# test(f, [0,0])
+	# test(f2, [0,0])
 	# test(schaffer, [0,0])
 	test(schaffer_n4, [0,1.25313])
 	# test(schaffer, [0,0])
