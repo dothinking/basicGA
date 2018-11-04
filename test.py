a108b4a8334ca85ad1ed699a74c83698bd3f4158
dothinking <train8808@gmail.com>
2018-11-04 22:30:37 +0800

    update test case

--- test.py
+++ test.py
@@ -7,18 +7,17 @@ from TestFun import *
 import time
 
 # objective
-f = Bukin()
+f = Himmelblau()
 
 # GA process
 s = time.time()
 I = Individual(f.ranges)
-P = Population(I, 50)
-S1 = RouletteWheelSelection()
-S2 = RankingSelection(0.33)
+P = Population(I, 100)
+S = RouletteWheelSelection()
 C = Crossover([0.6,0.9], 0.55)
 M = Mutation(0.1)
 
-g = GA(P, S2, C, M)
+g = GA(P, S, C, M)
 res = g.run(f.objective,500)
 
 # theoretical result
