3ff0843acd9e1b38c7707dd41b65603323853e4a
dothinking <train8808@gmail.com>
2018-11-04 23:00:22 +0800

    test cases from Wiki: test function for optimization

--- test.py
+++ test.py
@@ -1,31 +1,41 @@
 from GAComponents import Individual, Population
-from GAOperators import RouletteWheelSelection, RankingSelection, Crossover, Mutation
+from GAOperators import RouletteWheelSelection, Crossover, Mutation
 from GA import GA
-
 from TestFun import *
-
 import time
 
-# objective
-f = Himmelblau()
 
-# GA process
-s = time.time()
-I = Individual(f.ranges)
-P = Population(I, 100)
-S = RouletteWheelSelection()
-C = Crossover([0.6,0.9], 0.55)
-M = Mutation(0.1)
+def test(FUN):
 
-g = GA(P, S, C, M)
-res = g.run(f.objective,500)
+	s = time.time()
 
-# theoretical result
-print('\nGlobal solution input: {0}'.format(f.solution))
-print('Global solution output: {0}\n'.format(f.value))
+	# objective function
+	f = FUN()
 
-# GA result
-print('\nGA solution input: {0}'.format(res.solution))
-print('GA solution output: {0}\n'.format(res.evaluation))
+	# GA process	
+	I = Individual(f.ranges)
+	P = Population(I, 50)
+	S = RouletteWheelSelection()
+	C = Crossover([0.6,0.9], 0.55)
+	M = Mutation(0.1)
+	g = GA(P, S, C, M)
 
-print('\nspent time:', time.time()-s)
+	# solve
+	res = g.run(f.objective,500)
+
+	# theoretical result
+	print('---TEST FUNCTION: {0}---'.format(FUN.__name__))
+	print('Global solution input: {0}'.format(f.solution))
+	print('Global solution output: {0}'.format(f.value))
+
+	# GA result
+	print('GA solution input: {0}'.format(res.solution))
+	print('GA solution output: {0}'.format(res.evaluation))
+
+	print('spent time: {0}\n'.format(time.time()-s))
+
+if __name__ == '__main__':
+
+	FUNS = [Ackley, Beale, Booth, Bukin, Easom, Eggholder, GoldsteinPrice, Himmelblau, HolderTable, Matyas, McCormick, Rastrigin, Rosenbrock, Schaffer, Schaffer_N4, StyblinskiTang]
+	for fun in FUNS:
+		test(fun)
