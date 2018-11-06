92d4f1047be54956edc44c5d96b578005b34cd3d
dothinking <13885442+dothinking@users.noreply.github.com>
2018-11-06 17:27:53 +0800

    Create test.py

---
+++ examples/functions_test/test.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import time
+
+script_path = os.path.abspath(__file__) # current script path
+package_path = os.path.dirname(os.path.dirname(os.path.dirname(script_path)))
+sys.path.append(package_path)
+
+from GA.GAPopulation.DecimalIndividual import DecimalFloatIndividual
+from GA.GAPopulation.Population import Population
+from GA.GAOperators.Selection import RouletteWheelSelection, LinearRankingSelection
+from GA.GAOperators.Crossover import DecimalCrossover
+from GA.GAOperators.Mutation import DecimalMutation
+from GA.GA import GA
+
+from TestFun import *
+
+
+
+def test(FUN):
+
+	s = time.time()
+
+	# objective function
+	f = FUN()
+
+	# GA process
+	I = DecimalFloatIndividual(f.ranges)
+	P = Population(I, 50)
+	R = RouletteWheelSelection()
+	L = LinearRankingSelection(99)
+	C = DecimalCrossover([0.6,0.9], 0.55)
+	M = DecimalMutation(0.2)
+	g = GA(P, L, C, M)
+
+	# solve
+	res = g.run(f.objective, 500)
+
+	# theoretical result
+	print('---TEST FUNCTION: {0}---'.format(FUN.__name__))
+	print('Global solution input: {0}'.format(f.solution))
+	print('Global solution output: {0}'.format(f.value))
+
+	# GA result
+	print('GA solution input: {0}'.format(res.solution))
+	print('GA solution output: {0}'.format(res.evaluation))
+
+	print('spent time: {0}\n'.format(time.time()-s))
+
+if __name__ == '__main__':
+
+	FUNS = [Bukin, Eggholder, Rosenbrock]
+	for fun in FUNS:
+		test(fun)
