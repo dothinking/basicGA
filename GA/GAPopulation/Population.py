58108163db0c19e5c7f6ee3762dbff7f2d25e775
dothinking <13885442+dothinking@users.noreply.github.com>
2018-11-06 17:24:34 +0800

    Create Population.py

---
+++ GA/GAPopulation/Population.py
@@ -0,0 +1,50 @@
+#----------------------------------------------------------
+# Population class
+#----------------------------------------------------------
+import numpy as np
+
+class Population:
+	'''collection of individuals'''
+	def __init__(self, individual, size=50):
+		'''
+		individual   : individual template
+		size         : count of individuals		
+		'''
+		self.individual = individual
+		self.size = size
+		self.individuals = None
+
+
+	def initialize(self):
+		'''initialization for next generation'''
+		IndvClass = self.individual.__class__
+		self.individuals = np.array([IndvClass(self.individual.ranges) for i in range(self.size)], dtype=IndvClass)
+
+	def best(self, fun_evaluation, fun_fitness):
+		'''get best individual according to evaluation value '''
+		# evaluate first and collect evaluations
+		_, evaluation = self.evaluate(fun_evaluation, fun_fitness)
+		# get the minimum position
+		pos = np.argmin(evaluation)
+		return self.individuals[pos]
+
+	def evaluate(self, fun_evaluation, fun_fitness):
+		'''
+		calculate objectibe value and fitness for each individual.
+		fun_evaluation	: objective function
+		fun_fitness  	: population fitness based on evaluation
+		'''
+
+		# get the value directly if it has been calculated before
+		evaluation = np.array([fun_evaluation(I.solution) if I.evaluation is None else I.evaluation for I in self.individuals])
+
+		# calculate fitness
+		fitness = fun_fitness(evaluation)
+		fitness = fitness/np.sum(fitness) # normalize
+		
+		# set attributes for each individual
+		for I, e, f in zip(self.individuals, evaluation, fitness):
+			I.evaluation = e
+			I.fitness = f
+
+		return fitness, evaluation
