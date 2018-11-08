b1302f312c21724cd5523b97e7ece6fb8b1c4706
dothinking <13885442+dothinking@users.noreply.github.com>
2018-11-08 17:38:46 +0800

    Update SequenceIndividual.py

--- GA/GAPopulation/SequenceIndividual.py
+++ GA/GAPopulation/SequenceIndividual.py
@@ -7,9 +7,16 @@ from .Individual import Individual
 
 class UniqueSeqIndividual(Individual):
 	'''
-	sequence encoded individual: unique numbers in a certain order
-	ranges: int, e.g. ranges=5 -> 0,1,2,3,4
+	sequence encoded individual: unique numbers in a certain order	
 	'''
+
+	def __init__(self, ranges, loop=False):
+		'''
+		ranges: int, e.g. ranges=5 -> 0,1,2,3,4
+		loop: bool, if True: 0,1,2,3,4 = 1,2,3,4,0 = 3,4,0,1,2 = ...
+		'''
+		self.loop = loop
+		super().__init__(ranges)
 	
 	def init_solution(self, ranges):
 		'''
@@ -20,7 +27,38 @@ class UniqueSeqIndividual(Individual):
 
 		self._ranges = ranges
 		self._dimension = ranges
-		self._solution = np.random.choice(ranges, ranges, replace=False)
+		seq = np.random.choice(ranges, ranges, replace=False)
+		self._solution = self.unique_sequence(seq)
+		print(self._solution)
+
+	@property
+	def solution(self):
+		return self._solution
+
+	@solution.setter
+	def solution(self, solution):
+		self._solution = self.unique_sequence(solution)
+
+	def unique_sequence(self, sequence):
+		'''
+		only relative order is considered for a sequece loop, 
+		so represent the loop from element 0
+		e.g. 1,4,0,2,3 -> 0,2,3,1,4
+		'''
+
+		if not self.loop: # do nothing if the sequence is not a loop
+			return sequence
+
+		# find position of element 0
+		arg = np.argwhere(sequence==0) # [[3]]
+		pos = arg[0][0]
+
+		# exchange two sections seperated by element 0
+		unique_seq = np.empty_like(sequence)
+		unique_seq[0:-pos], unique_seq[-pos:] = sequence[pos:], sequence[0:pos]
+		
+		return unique_seq
+
 
 
 class ZeroOneSeqIndividual(Individual):
